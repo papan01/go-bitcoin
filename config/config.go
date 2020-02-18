e350eb77eca5f8edfd7562c7ea611dc0ffd54abd
papan01 <navy90517@gmail.com>
2020-02-19 00:26:27 +0800

    Refactoring

--- config/config.go
+++ config/config.go
@@ -8,11 +8,15 @@ import (
 
 //初始化設定檔
 func init() {
-	viper.SetConfigName("config")
-	viper.SetConfigType("json")
-	viper.AddConfigPath("./config")
-	err := viper.ReadInConfig() // Find and read the config file
-	if err != nil {             // Handle errors reading the config file
+	setConfig("config", "json", "./config")
+}
+
+func setConfig(name string, extension string, path string) {
+	viper.SetConfigName(name)
+	viper.SetConfigType(extension)
+	viper.AddConfigPath(path)
+	err := viper.ReadInConfig()
+	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s", err))
 	}
 }
